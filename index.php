24886b5faa6b298f768d2d6207097fbd22c5082d
UA\pberna <pbernardes@uaum.uminho.pt>
2020-10-29 18:39:40 +0000

    Correcção DBUpdate: upload imagens (JPG) e documentos (PDF) - o path foi alterado

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+
+      <title>RARAA</title>
+      <link rel="shortcut icon" href="Img/Logo/RARAA_1.ico" type="image/x-icon"/>
+      
+      <!-- Bootstrap core CSS -->
+      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Custom styles for this template -->
+      <link href="css/raraa-frontpage.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        
+    <!-- Navigation -->
+    <?php
+    include 'php_source/raraa_navbar.php';
+    ?>                
+
+        <!-- Header with Background Image -->
+        <header class="raraa-header">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                          <img class="d-block w-100" src="Img/Home/Header/1.jpg" alt="First slide">
+                      </div>
+                      <div class="carousel-item">
+                          <img class="d-block w-100" src="Img/Home/Header/2.jpg" alt="Second slide">
+                      </div>
+                      <div class="carousel-item">
+                          <img class="d-block w-100" src="Img/Home/Header/3.jpg" alt="Third slide">
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>  
+        </header>
+          
+
+        <!-- Page Content -->
+        <div class="container">
+
+          <div class="row">
+            <div class="col-sm-8">
+              <h2 class="mt-4">Working Progress</h2>
+              <img class="d-block w-100" src="http://placehold.it/1920x1080" alt="Third slide">
+            </div>
+            <div class="col-sm-4">
+              <h2 class="mt-4">News</h2>
+              <img class="d-block w-100" src="http://placehold.it/1920x800" alt="Third slide">
+            </div>
+          </div>
+          <!-- /.row -->
+
+
+          <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+        <hr>
+        <!-- Footer -->
+        <footer class="py-5 bg-dark">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-5 text-white">
+               <address>
+                <strong>Repositório de Arte Rupestre de Acesso Aberto</strong>
+                <br>Rua da Imaginação Fertil
+                <br>1234-567 Cidade Imaginária
+                <br>
+              </address>
+              </div>
+              <div class="col-sm-3 text-white">
+                <address>
+                    <br>
+                    T: ++ 351 (123) 456789
+                    <br>
+                    E: <a href="mailto:#">info@raraa.pt</a>
+              </address>  
+              </div>
+              <div class="col-sm-4">
+                  <br><br>
+                  <p class="m-0 text-center text-white">Copyright &copy; RARAA 2020</p></div>
+              </div>
+              </div>                 
+
+          <!-- /.container -->
+        </footer>
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+
+</html>
---
+++ php_source/raraa_navbar.php
@@ -0,0 +1,55 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img class="d-block w-100" src="Img/Logo/RARAA_Logo_149.png" alt="Logo">
+        </a>
+        <a class="navbar-brand" href="#">
+            Open Access Rock Art Repository
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                      <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Members</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Working Groups</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">
+                            WG 01
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">
+                            WG 02
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">
+                            WG 03
+                        </a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
